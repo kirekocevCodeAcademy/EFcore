a6f60eb6bfa239cb79e6b9122090cbad5054ddec
kirekocevCodeAcademy <kirekocevCodeAcademy@gmail.com>
2020-02-07 20:57:25 +0100

    Add project files.

---
+++ SamuraiApp.Data/SamuraiDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiDbContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (localdb)\\mssqllocaldb; Database = Samurai; Trusted_Connection = True; ");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(e => new { e.BattleId, e.SamuraiId });
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
---
+++ SamuraiApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using SamuraiApp.Domain;
+using System;
+using System.Linq;
+
+namespace SamuraiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
